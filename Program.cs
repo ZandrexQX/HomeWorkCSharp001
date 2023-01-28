82f01cd7e96b6b971e0e316e6153b17ed2baab30
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-01-28 17:11:38 +0300

    Task 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Task 1");
+Console.WriteLine("------");
+
+Console.Write("Enter first number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1>num2) Console.WriteLine($"{num1} more {num2}");
+else if (num1<num2) Console.WriteLine($"{num2} more {num1}");
+else
+    Console.WriteLine("The numbers are equal");
+
+Console.ReadLine();
+Console.Clear();
