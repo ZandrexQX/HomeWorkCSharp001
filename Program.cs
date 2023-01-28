3790b00c8b2070f3a22051a6c8634344b394910b
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-01-28 17:24:43 +0300

    Task 4

--- Program.cs
+++ Program.cs
@@ -14,3 +14,25 @@ else
 
 Console.ReadLine();
 Console.Clear();
+
+Console.WriteLine("Task 4");
+Console.WriteLine("------");
+
+Console.Write("Enter first number:");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number:");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter third number:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (num1>max) max = num1;
+if (num2>max) max = num2;
+if (num3>max) max = num3;
+Console.WriteLine($"Max = {max}");
+
+Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine("Task 6");
+Console.WriteLine("------");
